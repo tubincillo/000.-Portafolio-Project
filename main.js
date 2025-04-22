195462f94ed956c888cf57aa60d7a6dc0abfc209
tubincillo <tubincillo@gmail.com>
2025-04-22 09:33:19 -0400

    ConJS

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-let foto = document.getElementById('foto');
+let foto = document.getElementById('banner-main');
+
+function colores(){
+    return Math.floor(Math.random()*256);
+}
+
+function cambioColores(){
+
+    let coloresCambiados= 'rgb(' + colores() + ','  + colores() + ',' + colores() +')';
+    event.target.style.backgroundColor = coloresCambiados; 
+
+}
+
+foto.addEventListener('click',cambioColores);
